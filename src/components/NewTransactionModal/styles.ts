f6f75f97771f779ea011eef492a721f7878ca54f
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-04-02 18:21:28 -0300

    configurated forms

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -55,22 +55,36 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
 
-    button {
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
+`;
 
-        background: transparent;
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
 
-        display: flex;
-        align-items: center;
-        justify-content: center;
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D'
+};
 
-        transition: border-color 0.2s; 
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
 
-        &:hover {
-            border-color: ${darken(0.1, '#d7d7d7')};
-        }
+    background: ${(props) => props.isActive
+        ? transparentize(0.9, colors[props.activeColor])
+        : 'transparent'};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s; 
+
+     &:hover {
+        border-color: ${darken(0.1, '#d7d7d7')};
+     }
     }
 
     img {
@@ -82,6 +96,5 @@ export const TransactionTypeContainer = styled.div`
         display: inline-block;
         margin-left: 1rem;
         font-size: 1rem;
-        color: var(--text-title);       
-    }
+        color: var(--text-title); 
 `;
